e2d4723b93934251c67d47a5ba4120464ac9e65b
Maxim Alferov <malferov44@gmail.com>
2019-10-16 23:11:30 +0200

    fix health check

--- alb.tf
+++ alb.tf
@@ -24,7 +24,7 @@ resource "aws_lb_target_group" "tg" {
   vpc_id   = aws_vpc.vpc.id
   health_check {
     matcher = "200"
-    path    = "/"
+    path    = "/hc"
   }
   depends_on = [aws_lb.alb]
 }
